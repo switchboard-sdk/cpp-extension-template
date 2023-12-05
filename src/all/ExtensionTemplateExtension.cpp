9f16cbf49a3a6f3828d38eacacb06d4b8197dbe8
Balázs Kiss <dev@balazskiss.name>
2023-12-05 16:53:40 +0100

    Initial commit

---
+++ src/all/ExtensionTemplateExtension.cpp
@@ -0,0 +1,10 @@
+#include "ExtensionTemplateExtension.hpp"
+
+#include "Logger.hpp"
+
+namespace switchboard::extensions::extensiontemplate {
+
+void ExtensionTemplateExtension::initialize() {
+    Logger::debug("Initialized Switchboard Extension -- ExtensionTemplate --");
+}
+}
---
+++ src/all/ExtensionTemplateExtension.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace switchboard::extensions::extensiontemplate {
+
+class ExtensionTemplateExtension final {
+public:
+    static void initialize();
+};
+
+}
+
