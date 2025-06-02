986e11efbeb2f2875272e3c61934f0e05ea56b14
Balázs Kiss <dev@balazskiss.name>
2025-06-02 18:58:15 +0700

    Update FindSwitchboardSDK.cmake

--- CMake/FindSwitchboardSDK.cmake
+++ CMake/FindSwitchboardSDK.cmake
@@ -4,7 +4,7 @@
 set(SwitchboardSDK_FOUND FALSE)
 
 if(NOT DEFINED SWITCHBOARD_PACKAGE_VERSION)
-    set(SWITCHBOARD_PACKAGE_VERSION "2.3.1-beta") # Default version
+    set(SWITCHBOARD_PACKAGE_VERSION "2.3.1") # Default version
 endif()
 
 # Detect platform (adjust as needed)
